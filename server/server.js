f43b641331ddc00250cf8591dfb6dff86763b55a
Renzo Tomlinson <rttomlinson@gmail.com>
2017-05-19 18:15:30 +0000

    Add procfile to server dir

--- server/server.js
+++ server/server.js
@@ -1,5 +1,8 @@
 require("isomorphic-fetch");
-require("dotenv").config();
+
+if (process.env.NODE_ENV !== 'production') {
+    require("dotenv").config();
+}
 const express = require("express");
 const app = express();
 const moment = require("moment");
